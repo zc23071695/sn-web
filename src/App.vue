6813d95a0f554d91f080a93cd45c0e89a9c1525b
zc23071695 <zc23071695@outlook.com>
2020-05-13 19:25:12 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,49 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <h1>
+        Good Job
+      </h1>
     </div>
-    <router-view/>
   </div>
 </template>
+<script>
+export default {
+  name: 'app',
+  components: {},
+  data() {
+    return {
+      name: 'xiaoming',
+    }
+  },
+  methods: {
+    fn1(user) {
+      function Range(x) {
+        this.x = x
+      }
+      Range.prototype = {
+        includes: function() {
+          return true
+        },
+      }
+      Object.defineProperty(Object.prototype, 'num', {
+        value: 132,
+        enumerable: false,
+      })
 
+      var r = new Range(12)
+      console.log(Object.keys({ name: 'cc' }))
+      let obj = { name: 'cc' }
+      for (let i in obj) {
+        console.log(i + 'ww')
+      }
+    },
+  },
+  mounted() {
+    this.fn1()
+  },
+}
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -15,18 +51,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
